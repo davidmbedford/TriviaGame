3fc12c40523a1d459fa0341ce58db6084a276d0f
DavidmB <david.m.bedford1@gmail.com>
2019-03-18 16:55:45 -0400

    mar 18th, psuedocode added to js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Trivia game
+// A list of features I need to design
+////shows one question until:
+/////////the player answers it
+/////////or their time runs out.
+////when they hover over the answer, it is highlighted.
+////when they click, it will:
+////////show 'congrats' if they are correct
+////////show 'oops' if they are wrong. and show the correct answer.
+////////wait 3 seconds, move on to next question
+////////from directions:"The scenario is similar for wrong answers and time-outs.'
+////If they run out of time, tell the player:
+////////"time's up" and display the correct answer.
+////////Wait a few seconds, then show the next question.
+////On the final screen, show the number of correct answers,
+///////incorrect answers, and an option to restart the game
+///////(without reloading the page).
