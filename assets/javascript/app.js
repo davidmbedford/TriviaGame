9352aceace1c79772d259fc47a37e8e38f94b65e
DavidmB <david.m.bedford1@gmail.com>
2019-03-19 15:31:57 -0400

    mar 19th, first commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,28 +15,43 @@
 ////On the final screen, show the number of correct answers,
 ///////incorrect answers, and an option to restart the game
 ///////(without reloading the page).
+/////// first and last slide will contain a start and a restart button
 
-//This object contains all possibke answers... or it will
+window.onload = function() {
+  $("#correctStat").text(correct);
+  $("#wrongStat").text(wrong);
+  $("#scoreStat").text(score);
+}
+
+var correct = 0;
+var wrong = 0;
+var score = "";
+
+var count = 0; //we will need this to track the number of questions so that when
+              // count = total, we can trigger the endcard.
+
+var timerRunning = false; // we will need this to activsate the slideshow after we hit start
+
+//This object contains all possible questions, answers... or, soon it will
 var allQuestions = [
   questionOne = {
-    qOne: "Is it a question!?";
-    aOne: "Yars";
-    aTwo: "Nar!";
+    qOne: "Is it a question!?",
+    aOne: "Yars",
+    aTwo: "Nar!",
     aThree: "Maybe"
-  }
+  },
 
   questionTwo = {
-    qOne: "Is a dog a duck?";
-    aOne: "Perhaps if not";
-    aTwo: "No, ducks dont have paws";
-    aThree: "Yes, dogs have bills";
-  }
+    qOne: "Is a dog a duck?",
+    aOne: "Perhaps if not",
+    aTwo: "No, ducks dont have paws",
+    aThree: "Yes, dogs have paws"
+  },
 
   questionThree = {
-    qOne: "Where is the money?";
-    aOne: "cmon, tony, i know you know where the money is";
-    aTwo: "listen, you got two minutes before I smash this large container of jello over your head, i aint kiddin tony!";
-    aThree: "No, dogs have bills";
+    qOne: "Where is the money?",
+    aOne: "cmon, tony, i know you know where the money is",
+    aTwo: "listen, you got two minutes before I smash this large container of jello over your head, i aint kiddin tony!",
+    aThree: "No, dogs have bills"
   }
-
-]
+];
